aa8f086fe36001f4b42bf1b7530f405ef31955fe
Ivan Chuchulski <zake6@abv.bg>
2020-04-28 15:31:30 +0300

    add solution

--- leetcode-april-challenge/april-03-maximum-subarray/maximum-subarray.cpp
+++ leetcode-april-challenge/april-03-maximum-subarray/maximum-subarray.cpp
@@ -8,16 +8,19 @@ using namespace std;
 
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int maxSum = INT_MIN;
-        int current = 0;
-
-        for (int num : nums) {
-            
-
+    // Kadane's algorithm, but because there can be negative 
+    // numbers consider the answer to be a the negative number
+    // that's why line 20 and 21 have to be in that order
+    int maxSubArray(vector<int>& nums) {        
+        int largestSum = INT_MIN;
+        int candidateSum = 0;
+        
+        for (int number : nums) {
+            candidateSum += number;
+            largestSum = std::max(largestSum, candidateSum);
+            candidateSum = std::max(candidateSum, 0);
         }
 
-
+        return largestSum;
     }
-
 };
